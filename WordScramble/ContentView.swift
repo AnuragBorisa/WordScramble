facd7228b96a3ee4bd562f7320ac87f635fb76e6
Anurag Borisa <anuragborisa@gmail.com>
2025-01-05 14:08:10 +0530

    completed

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -14,31 +14,47 @@ struct ContentView: View {
     @State private var errorTitle = ""
     @State private var errorMessage = ""
     @State private var showingError = false
+    @State private var playerScore = 0;
     
     func addNewWord(){
         let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
         
         guard answer.count > 0 else {return}
         
+        guard answer.count >= 3 else {
+            wordError(title: "Word too short", message: "Words must be at least 3 characters long")
+            
+            return
+        }
+        
+        guard answer != rootWord else{
+            wordError(title: "You cant use the same word", message: "Use Different Words")
+            playerScore-=1
+            return
+        }
+        
         guard isOriginal(word:answer) else {
             wordError(title: "Word used already",message: "Be more orignal")
+            playerScore-=1
             return
         }
         
         guard isPossible(word:answer) else {
             wordError(title: "Word not possible",message: "You can't spell that word from '\(rootWord)'!")
+            playerScore-=1
             return
         }
         
         guard isReal(word:answer) else {
             wordError(title: "Word not recognized",message: "You can't just make them up,you know!")
+            playerScore-=1
             return
         }
         
         withAnimation{
             usedWords.insert(answer,at: 0)
         }
-       
+        playerScore += 1
         newWord = ""
     }
     
@@ -53,6 +69,7 @@ struct ContentView: View {
         }
         fatalError("Could not load start.txt from bundle")
     }
+    
     
     func isOriginal(word:String) -> Bool {
         !usedWords.contains(word)
@@ -101,6 +118,9 @@ struct ContentView: View {
                          
                     }
                 }
+                Section {
+                    Text("Your score is \(playerScore)")
+                }
             }
             .navigationTitle(rootWord)
             .onSubmit(addNewWord)
@@ -111,6 +131,11 @@ struct ContentView: View {
                 Button("OK"){}
             } message:{
                 Text(errorMessage)
+            }
+            .toolbar{
+                Button("New Game"){
+                    startGame()
+                }
             }
         }
     }
